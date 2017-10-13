552b73b6a73e4f9d956962ad86c579a68ce351ce
Paulius Bieksa <priverstinis@gmail.com>
2017-10-13 15:52:23 +0100

    Cloth.

--- physics simulation/main.cpp
+++ physics simulation/main.cpp
@@ -73,37 +73,101 @@ int main()
 	Shader shader_particle = Shader("resources/shaders/core.vert", "resources/shaders/core_blue.frag");
 	Shader shader_yellow = Shader("resources/shaders/core.vert", "resources/shaders/core_yellow.frag");
 
-	std::vector<Particle> anchors;
-	for (int i = 0; i < 4; i++)
+	//std::vector<Particle> anchors;
+	//for (int i = 0; i < 4; i++)
+	//{
+	//	anchors.push_back(Particle());
+	//	anchors[i].getMesh().setShader(shader_yellow);
+	//	anchors[i].setPos(glm::vec3(-2.5f * powf(-1.0f, (float)i), 4.5f, i > 1 ? 2.5f : -2.5f));
+	//}
+
+	////Particle debug = Particle();
+	////debug.setPos(glm::vec3(0.0f, 4.5f, 0.0f));
+	////debug.getMesh().setShader(Shader("resources/shaders/core.vert", "resources/shaders/core_red.frag"));
+
+	//// Set particle parameters
+	//for (int i = 0; i < 8; i++)
+	//{
+	//	particles.push_back(Particle());
+	//	particles[i].translate(glm::vec3(-2.0f + (float)i / 2.0f, 4.5f, 0.0f));
+	//	particles[i].getMesh().setShader(shader_particle);
+	//	particles[i].setMass(0.1f);
+	//	particles[i].addForce(new Gravity());
+	//	particles[i].addForce(new Drag());
+	//}
+
+	for (int i = 0; i < 10; i++)
 	{
-		anchors.push_back(Particle());
-		anchors[i].getMesh().setShader(shader_yellow);
-		anchors[i].setPos(glm::vec3(-2.5f * powf(-1.0f, (float)i), 4.5f, i > 1 ? 2.5f : -2.5f));
+		for (int j = 0; j < 10; j++)
+		{
+			particles.push_back(Particle());
+			particles[i * 10 + j].translate(glm::vec3(-2.5f + (float)i / 2.0f, 4.5f, 2.5f - (float)j / 2.0f));
+			if ((i == 0 && j == 0) || (i == 9 && j == 0) || (i == 0 && j == 9) || (i == 9 && j == 9))
+			{
+				particles[i * 10 + j].getMesh().setShader(shader_yellow);
+				continue;
+			}
+			particles[i * 10 + j].getMesh().setShader(shader_particle);
+			particles[i * 10 + j].setMass(0.1f);
+			particles[i * 10 + j].addForce(new Gravity());
+			particles[i * 10 + j].addForce(new Drag());
+		}
 	}
 
-	//Particle debug = Particle();
-	//debug.setPos(glm::vec3(0.0f, 4.5f, 0.0f));
-	//debug.getMesh().setShader(Shader("resources/shaders/core.vert", "resources/shaders/core_red.frag"));
+	/*particles[0].addForce(new Hooke(&anchors[0], 5.0f, 1.5f, 1.0f));
+	particles[7].addForce(new Hooke(&anchors[1], 5.0f, 1.5f, 1.0f));*/
 
-	// Set particle parameters
-	for (int i = 0; i < 8; i++)
-	{
-		particles.push_back(Particle());
-		particles[i].translate(glm::vec3(-2.0f + (float)i / 2.0f, 4.5f, 0.0f));
-		particles[i].getMesh().setShader(shader_particle);
-		particles[i].setMass(0.1f);
-		particles[i].addForce(new Gravity());
-		particles[i].addForce(new Drag());
-	}
-
-	particles[0].addForce(new Hooke(&anchors[0], 5.0f, 1.5f, 1.0f));
-	particles[7].addForce(new Hooke(&anchors[1], 5.0f, 1.5f, 1.0f));
-
-	for (int i = 0; i < particles.size() - 1; i++)
+	/*for (int i = 0; i < particles.size() - 1; i++)
 	{
 		particles[i].addForce(new Hooke(&particles[i + 1], 5.0f, 1.0f, 1.0f));
 		particles[i + 1].addForce(new Hooke(&particles[i], 5.0f, 1.0f, 1.0f));
+	}*/
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (i == 0)
+			{
+				if (j == 0 || j == 9)
+					continue;
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j - 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j + 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i + 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+			}
+			else if (i == 9)
+			{
+				if (j == 0 || j == 9)
+					continue;
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j - 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j + 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i - 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+			}
+			else if (j == 0)
+			{
+				if (i == 0 || i == 9)
+					continue;
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j + 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i + 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i - 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+			}
+			else if (j == 9)
+			{
+				if (i == 0 || i == 9)
+					continue;
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j - 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i + 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i - 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+			}
+			else
+			{
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j - 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[i * 10 + j + 1], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i + 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+				particles[i * 10 + j].addForce(new Hooke(&particles[(i - 1) * 10 + j], 15.0f, 1.0f, 0.3f));
+			}
+		}
 	}
+
 
 	// Room corners
 	glm::vec3 roomCorner1 = glm::vec3(-10.0f, 0.0f, -10.0f);
@@ -116,7 +180,7 @@ int main()
 	// Time stuff
 	double time = 0.0;
 	//double dt = 1.0 / 60.0;
-	double dt = 0.0002;
+	double dt = 0.01;
 	double currentTime = (double)glfwGetTime();
 	double timeAccumulator = 0.0;
 
@@ -207,8 +271,6 @@ int main()
 		// draw groud plane
 		app.draw(plane);
 		// draw particles
-		for (Particle a : anchors)
-			app.draw(a.getMesh());
 		for (Particle p : particles)
 			app.draw(p.getMesh());
 		//	app.draw(debug.getMesh());
